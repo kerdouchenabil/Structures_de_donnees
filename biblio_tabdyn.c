5b78fa1ff5de95838fc640437ef635e5f085ea67
ASSIREM <assirem200@live.fr>
2020-03-16 03:01:11 +0100

    je sais pas

--- biblio_tabdyn.c
+++ biblio_tabdyn.c
@@ -4,13 +4,13 @@
 #include <stdio.h>
 
 
-Biblio *nouvelle_biblio(void) 
+Biblio *nouvelle_biblio(void)
 {
    Biblio* bib= malloc(sizeof(Biblio));
    bib->nE=0;
    bib->capacite=50;
-   bib->T= malloc( (bib->capacite)*sizeof(CellMorceau));
-   
+   bib->T= malloc( (bib->capacite)*sizeof(CellMorceau ));
+
    return bib;
 }
 
@@ -23,11 +23,12 @@ void libere_morceau(CellMorceau* cm){
 
 
 void libere_biblio(Biblio *B)
-{	
+{
 	for(int i=0; i< B->nE; i++){
-		libere_morceau(B+i); //libere_morceau(B->T[i]); ??
+		libere_morceau(&(B->T[i])); /*si probleme a l'execution changer
+        l'argument et mettre des . a la place des fleches  */
 	}
-	
+
    free(B->T);
    free(B);
 }
@@ -38,26 +39,26 @@ void insere(Biblio *B, int num, char *titre, char *artiste)
     if(!B){
     	B= nouvelle_biblio();
     }
-    
+
     if(B->nE >= B->capacite){
-    	realloc( B->T , 2*(B->capacite)*sizeof(CellMorceau) );
+    	int * p =(int*)realloc( B->T , 2*(B->capacite)*sizeof(CellMorceau ) );
     	printf("reallocation\n");
     }
-    
+
     //creation du morceau
-    CellMorceau* cm = malloc(sizeof(CellMorceau));
+    CellMorceau * cm = malloc(sizeof(CellMorceau ));
     cm->titre=titre;
     cm->artiste=artiste;
     cm->num=num;
-    
+
     //insertion dans la case libre (nE)
-    B->T[B->nE] = cm; 
+    B->T[B->nE] = *cm;
     B->nE = B->nE+1;
-    
+
 }
 
 
-void afficheMorceau(CellMorceau *cell)
+void afficheMorceau(CellMorceau  *cell)
 {
 	printf("*T* %8d * %-32.32s * %-32.32s *T*\n", cell->num, cell->titre, cell->artiste);
 }
@@ -67,18 +68,18 @@ void afficheMorceau(CellMorceau *cell)
 void affiche(Biblio *B)
 {
 	for(int i=0; i<B->nE; i++){
-		afficheMorceau(B->T[i]);
+		afficheMorceau(&B->T[i]);
 	}
 }
 
 
 
 
-CellMorceau * rechercheParNum(Biblio *B, int num)
+CellMorceau  * rechercheParNum(Biblio *B, int num)
 {
 	for(int i=0; i< B->nE; i++){
-		if(B->T[i]->num  ==  num ){
-			return B->T[i];
+		if(B->T[i].num  ==  num ){
+			return &B->T[i];
 		}
 	}
 	printf("morceau non trouvé\n");
@@ -86,11 +87,11 @@ CellMorceau * rechercheParNum(Biblio *B, int num)
 }
 
 
-CellMorceau *rechercheParTitre(Biblio *B, char * titre)
+CellMorceau  *rechercheParTitre(Biblio *B, char * titre)
 {
 	for(int i=0; i< B->nE; i++){
-		if(strcmp(B->T[i]->titre , titre)  ==  0 ){
-			return B->T[i];
+		if(strcmp(B->T[i].titre , titre)  ==  0 ){
+			return &B->T[i];
 		}
 	}
 	printf("morceau non trouvé\n");
@@ -100,12 +101,12 @@ CellMorceau *rechercheParTitre(Biblio *B, char * titre)
 
 
 
-CellMorceau * rechercheParNum_indice(Biblio *B, int num, int* indice)
+CellMorceau  * rechercheParNum_indice(Biblio *B, int num, int* indice)
 {
 	for(int i=0; i< B->nE; i++){
-		if(B->T[i]->num  ==  num ){
+		if(B->T[i].num  ==  num ){
 			*indice= i;
-			return B->T[i];
+			return &B->T[i];
 		}
 	}
 	printf("morceau non trouvé\n");
@@ -114,27 +115,27 @@ CellMorceau * rechercheParNum_indice(Biblio *B, int num, int* indice)
 
 
 int supprimeMorceau(Biblio *B, int num)
-{	
+{
 	int ind;
-	CellMorceau* cm = rechercheParNum_indice(B, num, &ind);
-	
+	CellMorceau * cm = rechercheParNum_indice(B, num, &ind);
+
 	if(cm){ //element a supprimer trouvé
 		//supprimer le morceau
 		libere_morceau(cm); // cm == B->T[ind]
-	
+
 		//decaler tous les autres morceaux
 		for(int i=ind; i< (B->nE)-1; i++){  //pour aller jusqua T[B->nE-2]
 			B->T[i] = B->T[i+1];
 		}
 		//a la fin on supprime le dernier morceau en doublon
-		libere_morceau(B->T[B->nE-1]);
+		libere_morceau(&B->T[B->nE-1]);
 		//nE--
 		B->nE = B->nE -1;
-		
+
 		printf("morceau supprimé\n");
 		return 1;
 	}
-	
+
 	printf("morceau non trouvé\n");
 	return 0;
 }
@@ -142,8 +143,8 @@ int supprimeMorceau(Biblio *B, int num)
 
 
 void insereSansNum(Biblio *B, char *titre, char *artiste)
-{	
-	n = B->nE;
+{
+	int n = B->nE;
 	insere(B, n, titre, artiste);
 }
 
@@ -153,9 +154,20 @@ void insereSansNum(Biblio *B, char *titre, char *artiste)
 
 Biblio *extraireMorceauxDe(Biblio *B, char * artiste)
 {
-
+    if(!B){
+        return NULL;
+    }
+    if(!artiste){
+        return NULL ;
+    }
+    Biblio *bib  = nouvelle_biblio();
+    for(int i =0 ; i< B->nE;i++){
+        if(strcmp((B->T[i].artiste),artiste)==0){
+            insereSansNum(bib,strdup(B->T[i].titre),strdup(B->T[i].artiste));
+        }
+    }
+    return bib;
 }
-
 
 
 
@@ -163,36 +175,12 @@ Biblio *extraireMorceauxDe(Biblio *B, char * artiste)
 
 Biblio *uniques (Biblio *B)
 {
-   
+
+   for(int i = 0 ; i<B->nE-1;i++){
+     CellMorceau  * m = rechercheParTitre(B,B->T[i].titre);
+     if (m) {
+         supprimeMorceau(B,m->num);
+     }
+   }
+   return B ;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
